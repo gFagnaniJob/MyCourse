10e9c08acd9ce19f7c4a7bf12f53b5223769dc1d
Giuseppe Fagnani <gfagnani.job@gmail.com>
2019-04-29 15:04:06 +0200

    lettura sicura dati da db (no sql-injection)

--- Models/Services/Application/AdoNetCourseService.cs
+++ Models/Services/Application/AdoNetCourseService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
 using MyCourse.Models.Services.Infrastructure;
@@ -15,12 +16,33 @@ namespace MyCourse.Models.Services.Application
     }
     public CourseDetailViewModel GetCourse(int id)
     {
-      throw new System.NotImplementedException();
+      FormattableString query = $@"SELECT Id, Title, Description, ImagePath, Author, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses WHERE Id={id}
+      ; SELECT Id, Title, Description, Duration FROM Lessons WHERE CourseId={id}";
+
+      DataSet dataSet = db.Query(query);
+
+      //Course
+      var courseTable = dataSet.Tables[0];
+      if (courseTable.Rows.Count != 1)
+      {
+        throw new InvalidOperationException($"Did not return exactly 1 row for Course {id}");
+      }
+      var courseRow = courseTable.Rows[0];
+      var courseDetailViewModel = CourseDetailViewModel.FromDataRow(courseRow);
+
+      //Course lessons
+      var lessonDataTable = dataSet.Tables[1];
+      foreach(DataRow lessonRow in lessonDataTable.Rows)
+      {
+        LessonViewModel lessonViewModel = LessonViewModel.FromDataRow(lessonRow);
+        courseDetailViewModel.Lessons.Add(lessonViewModel);
+      }
+      return courseDetailViewModel;
     }
 
     public List<CourseViewModel> GetCourses()
     {
-      string query = "SELECT Id, Title, ImagePath, Author, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
+      FormattableString query = $"SELECT Id, Title, ImagePath, Author, Rating, FullPrice_Amount, FullPrice_Currency, CurrentPrice_Amount, CurrentPrice_Currency FROM Courses";
       DataSet dataSet = db.Query(query);
       var dataTable = dataSet.Tables[0];
       var courseList = new List<CourseViewModel>();
--- Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
+++ Models/Services/Infrastructure/SqliteDatabaseAccessor.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Data;
 using Microsoft.Data.Sqlite;
 
@@ -5,20 +7,36 @@ namespace MyCourse.Models.Services.Infrastructure
 {
   public class SqliteDatabaseAccessor : IDatabaseAccessor
   {
-    public DataSet Query(string query)
+    public DataSet Query(FormattableString formattableQuery)
     {
+      var queryArguments = formattableQuery.GetArguments();
+      var sqliteParameters = new List<SqliteParameter>();
+      for (var i=0; i<queryArguments.Length; i++)
+      {
+        var parameter = new SqliteParameter(i.ToString(), queryArguments[i]);
+        sqliteParameters.Add(parameter);
+        queryArguments[i] = "@" + i;
+      }
+      string query = formattableQuery.ToString();
+
       using (var dbConnection = new SqliteConnection("Data Source=Data/MyCourse.db"))
       {
         dbConnection.Open();
         using (var cmd = new SqliteCommand(query, dbConnection))
         {
+          cmd.Parameters.AddRange(sqliteParameters);
           using (var reader = cmd.ExecuteReader())
           {
             var dataSet = new DataSet();
             dataSet.EnforceConstraints = false;
-            var dataTable = new DataTable();
-            dataSet.Tables.Add(dataTable);
-            dataTable.Load(reader);
+            
+            do
+            {
+              var dataTable = new DataTable();
+              dataSet.Tables.Add(dataTable);
+              dataTable.Load(reader);
+            } while (!reader.IsClosed);
+
             return dataSet;
           }
         }
